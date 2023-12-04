3e999a3a89fe46f61f255a5b709144c277f06d0a
Andri Sunardi <info@andrisunardi.com>
2023-12-04 22:51:05 +0700

    Add Alert Components

---
+++ src/views/components/alert.blade.php
@@ -0,0 +1,24 @@
+@props([
+    'color' => 'primary',
+    'key' => null,
+    'text' => null,
+    'icon' => null,
+    'class' => null,
+    'loader' => false,
+    'dismissible' => false,
+    'fade' => false,
+    'show' => false,
+])
+
+<div @if ($loader) wire:loading.block wire:target="{{ $key }}" @endif>
+    <div class="alert alert-{{ $color }} {{ $dismissible ? 'alert-dismissible' : null }} {{ $fade ? 'fade' : null }} {{ $show ? 'show' : null }} d-flex align-items-center {{ $class }}"
+        role="alert">
+        @if ($icon)
+            <span class="{{ $icon }} fa-fw me-2"></span>
+        @endif
+
+        <div>
+            {{ $text }}
+        </div>
+    </div>
+</div>
