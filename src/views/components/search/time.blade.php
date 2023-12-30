471bd21707a863ab1ec5f91ffa310e8bc460b6fa
Andri Sunardi <info@andrisunardi.com>
2023-12-30 17:28:34 +0700

    Update Components Form Search Time

--- src/views/components/search/time.blade.php
+++ src/views/components/search/time.blade.php
@@ -1,12 +1,16 @@
 @props([
+    'class' => null,
+    'id' => null,
     'key' => 'time',
     'title' => trans('validation.attributes.time'),
     'icon' => 'fas fa-clock',
     'type' => 'time',
     'step' => 1,
+    'required' => false,
     'label' => true,
-    'required' => true,
+    'disabled' => false,
+    'helper' => null,
 ])
 
-<x-components::search :key="$key" :title="$title" :icon="$icon" :type="$type" :step="$step"
-    :label="$label" :required="$required" />
+<x-components::form.input :class="$class" :id="$id" :key="$key" :title="$title" :icon="$icon"
+    :type="$type" :step="$step" :required="$required" :label="$label" :disabled="$disabled" :helper="$helper" />
