655b92d42a3bbf731a4a95cf1251ee7aacf1719f
Andri Sunardi <info@andrisunardi.com>
2024-01-08 23:24:43 +0700

    Update Components Button And Add Disabled

--- src/views/components/button/void.blade.php
+++ src/views/components/button/void.blade.php
@@ -6,8 +6,9 @@
     'color' => 'danger',
     'size' => 'sm',
     'width' => '100',
+    'disabled' => false,
     'confirm' => false,
 ])
 
 <x-components::button :class="$class" :key="$key" :text="$text" :icon="$icon" :color="$color"
-    :size="$size" :width="$width" :confirm="$confirm" />
+    :size="$size" :width="$width" :disabled="$disabled" :confirm="$confirm" />
