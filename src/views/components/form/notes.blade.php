b3a3684133eedf9a98bb00170474103c4b3d6b3f
Andri Sunardi <info@andrisunardi.com>
2024-01-10 23:07:20 +0700

    Add - Components - Form - Notes

---
+++ src/views/components/form/notes.blade.php
@@ -0,0 +1,21 @@
+@props([
+    'class' => null,
+    'id' => null,
+    'key' => 'notes',
+    'title' => trans('validation.attributes.notes'),
+    'icon' => 'fas fa-file-lines',
+    'minlength' => 1,
+    'maxlength' => 65535,
+    'rows' => null,
+    'required' => false,
+    'label' => true,
+    'autocapitalize' => 'on',
+    'autofocus' => false,
+    'readonly' => false,
+    'disabled' => false,
+    'helper' => null,
+])
+
+<x-components::form.textarea :class="$class" :key="$key" :title="$title" :icon="$icon" :minlength="$minlength"
+    :maxlength="$maxlength" :rows="$rows" :required="$required" :label="$label" :autocapitalize="$autocapitalize" :autofocus="$autofocus"
+    :readonly="$readonly" :disabled="$disabled" :helper="$helper" />
