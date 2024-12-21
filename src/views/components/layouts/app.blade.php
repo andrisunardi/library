4b5fd38aebc4d1f9588ed7b9923ec8d4172b2593
Andri Sunardi <account@andrisunardi.com>
2024-12-22 00:46:37 +0700

    Update - Layouts - App - Remove - CMS

--- src/views/components/layouts/app.blade.php
+++ src/views/components/layouts/app.blade.php
@@ -14,7 +14,7 @@
         {{ env('APP_TITLE') }}
     </title>
 
-    @if (Route::is('cms.*') || explode('.', request()->getHost())[1] == 'cms')
+    @if (Route::is('cms.*'))
         @include('layouts.cms.vendors')
     @else
         @include('layouts.vendors')
@@ -22,7 +22,7 @@
 
     @stack('css')
 
-    @if (Route::is('cms.*') || explode('.', request()->getHost())[1] == 'cms')
+    @if (Route::is('cms.*'))
         @include('layouts.cms.css')
     @else
         @include('layouts.css')
@@ -30,13 +30,13 @@
 </head>
 
 <body>
-    @if (Route::is('cms.*') || explode('.', request()->getHost())[1] == 'cms')
+    @if (Route::is('cms.*'))
         @include('layouts.cms.body')
     @else
         @include('layouts.body')
     @endif
 
-    @if (Route::is('cms.*') || explode('.', request()->getHost())[1] == 'cms')
+    @if (Route::is('cms.*'))
         @include('layouts.cms.script')
     @else
         @include('layouts.script')
